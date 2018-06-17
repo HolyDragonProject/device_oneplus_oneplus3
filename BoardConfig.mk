5a8e8da850378068b28daa023fd9ab96bccb850e
Miguel Martinez <slverwolf@gmail.com>
2018-06-17 15:43:01 -0500

    Enable SDClang
    Change-Id: I0bc08943543ca8fea676bbe8706b50b8e051150f
    Signed-off-by: Miguel Martinez <slverwolf@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,8 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_USE_SDCLANG := true
+
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
